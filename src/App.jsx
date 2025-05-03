e2330bc6fdca453f8690d943620555881f65a7ea
elaaayech <nourkaibi70@gmail.com>
2025-05-03 23:35:46 +0200

    w5 arrow and handler functions in react

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {usestate} from 'react';
+const students = [
+  { id: 1, name: "Sara", age: 21 },
+  { id: 2, name: "Ali", age: 22 },
+  { id: 3, name: "nour", age:20},
+];
+const App = () => {
+  const [searchTerm, setSearchTerm] = useState('');
 
-function App() {
-  const [count, setCount] = useState(0)
+  const handleSearch = (event) => {
+    setSearchTerm(event.target.value);
+    console.log(event.target.value);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <h1>Student List</h1>
+      <input 
+        type="text" 
+        placeholder="Search students..." 
+        onChange={handleSearch} 
+      />
+      <ul>
+        {students
+          .filter(student => 
+            student.name.toLowerCase().includes(searchTerm.toLowerCase())
+          .map((student) => (
+            <li key={student.id}>
+              {student.name} - Age: {student.age}
+            </li>
+          ))
+        }
+      </ul>
+    </div>
+  );
 }
 
-export default App
+export default App;
+
